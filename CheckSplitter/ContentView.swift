ac0d5a9d475c5ef9f1a7c50125d705631e9ae0ce
Jacob-Hoff-man <cubhoffman@gmail.com>
2020-10-17 17:30:47 -0400

    version 1.1

--- CheckSplitter/ContentView.swift
+++ CheckSplitter/ContentView.swift
@@ -10,32 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var billValue = ""
-    @State private var numberOfPeople = 1
+    @State private var numberOfPeople = ""
     @State private var tipPercentage = 0
     
     let tipPercentages = [0, 5, 10, 15, 20, 25]
     
-    var totalPerPerson: Double {
+    var totalBill: Double {
         let billAmount = Double(billValue) ?? 0
-        let peopleCount = Double(numberOfPeople + 1)
         let tip = billAmount / 100 * Double(tipPercentages[tipPercentage])
-        let total = billAmount + tip
-        
-        return total / peopleCount
+        return billAmount + tip
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) ?? 1
+        return totalBill / peopleCount
     }
     
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Select Bill Amount and Number of People")) {
+                Section(header: Text("Enter Bill Amount and Number of People")) {
                     TextField("Bill Amount:", text: $billValue)
                         .keyboardType(UIKeyboardType.decimalPad)
                     
-                    Picker("Number of People:", selection: $numberOfPeople) {
-                        ForEach(1 ..< 100) {
-                            Text("\($0) People")
-                        }
-                    }
+                    TextField("Number of People:", text: $numberOfPeople)
+                        .keyboardType(UIKeyboardType.asciiCapableNumberPad)
                     
                 }
                 
@@ -48,13 +47,20 @@ struct ContentView: View {
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 
+                Section(header: Text("Total bill")) {
+                    Text("$\(totalBill, specifier: "%.2f")")
+                }
+                
                 Section(header: Text("Total Per Person")) {
                     Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
                 
             }
+            .foregroundColor(Color.green)
             .navigationBarTitle("Bill Splitter", displayMode: .inline)
+            .padding()
         }
+        .colorScheme(ColorScheme.dark)
     }
     
 }
